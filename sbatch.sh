157257ee1521eb58a06f5101ff8cb6e544a63a64
Colin Doumont <colin@principiae.be>
2023-12-02 11:11:13 +0100

    Minor fix

--- sbatch.sh
+++ sbatch.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpumem:32g
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=32G
-#SBATCH --output=log3.out
+#SBATCH --output=log.out
 
 source /cluster/home/cdoumont/miniconda3/etc/profile.d/conda.sh
 conda activate naslib
